1ec26c3373f4d776c99bbec00ef7b7f1a18a9340
parakh80 <patelparakh80@gmail.com>
2024-03-17 11:58:13 +0530

    update in bac

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -5,7 +5,7 @@ import Product from "../models/product.js";
 const seedProducts = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://ghulamabbas:ghulamabbas@shopit.wkd8107.mongodb.net/shopit-v2?retryWrites=true&w=majority"
+      "mongodb+srv://Parakh:Parakh%40123@cluster0.nb6gbzc.mongodb.net/AtoZShop?retryWrites=true&w=majority"
     );
 
     await Product.deleteMany();
